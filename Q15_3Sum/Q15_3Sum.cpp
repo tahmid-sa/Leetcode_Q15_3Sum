89a953c760a1ebe16f1f5ff7c2eb7e4541a027b0
Tahmid Sajin <tahmid.sajin.1999@gmail.com>
2022-07-06 01:32:45 -0400

    Adding files to github

---
+++ Q15_3Sum/Q15_3Sum.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> op;
+
+    if (nums.size() < 3) { //if number of elements inside nums is less than 3 return empty array.
+        return op;
+    }
+
+    sort(nums.begin(), nums.end());
+
+    for (int i = 0; i < nums.size() - 2; i++) {
+
+        if (i > 0 && nums[i] == nums[i - 1]) { //ignore dublicates for i to optimize the code
+            continue;
+        }
+
+        int j = i + 1, k = nums.size() - 1;
+        while (j < k) {
+            int sum = nums[i] + nums[j] + nums[k];
+
+            if (sum == 0) {
+                op.push_back({ nums[i], nums[j], nums[k] }); //if sum equal to zero push back the values
+                j++;
+                k--;
+
+                while (j < k && nums[j] == nums[j - 1]) j++; // Remove dublicates
+                while (j < k && nums[k] == nums[k + 1]) k--; // Remove dublicates
+            }
+            else if (sum < 0)
+                j++;    //if sum is negative increment j
+            else
+                k--;    //if sum is positive increment k
+        }
+    }
+    return op;
+}
+
+int main()
+{
+    vector<int> nums = { -1,0,1,2,-1,-4 };
+
+    vector<vector<int>> returned = threeSum(nums);
+
+    for (auto &i : returned) {
+        for (auto& j : i) {
+            cout << j << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
